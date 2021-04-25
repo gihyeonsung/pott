c81848f82cae4b2951b1e730bf74ba37549eb9dc
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-04-25 13:48:59 +0900

    feat: build input documents at once

--- cmd/pott/pott.go
+++ cmd/pott/pott.go
@@ -1,27 +1,40 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path"
 
 	"github.com/gihyeonsung/pott/internal/parser"
 	"github.com/gihyeonsung/pott/internal/renderer"
 )
 
-func main() {
-	file, err := os.Open(os.Args[1])
+const (
+	inputDir  = "input"
+	outputDir = "output"
+)
+
+func build(filename string) error {
+	inputPath := path.Join(inputDir, filename)
+	inputFile, err := os.Open(inputPath)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "could not read the document: %s", err.Error())
-		os.Exit(1)
+		return errors.New("could not read the document: " + err.Error())
 	}
 
-	doc, err := parser.Parse(file)
+	doc, err := parser.Parse(inputFile)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "could not parse the document: %s", err.Error())
-		os.Exit(1)
+		return errors.New("could not parse the document: " + err.Error())
 	}
 
-	fmt.Printf(`<!DOCTYPE html>
+	outputPath := path.Join(outputDir, filename+".html")
+	outputFile, err := os.Create(outputPath)
+	if err != nil {
+		return errors.New("could not create an output file: " + err.Error())
+	}
+
+	fmt.Fprintf(outputFile, `<!DOCTYPE html>
 <html>
 <head>
 <meta charset="utf-8">
@@ -29,7 +42,20 @@ func main() {
 <link rel="stylesheet" href="index.css" type="text/css">
 </head>
 <body>`)
-	renderer.Render(os.Stdout, doc)
-	fmt.Printf(`</body>
+	renderer.Render(outputFile, doc)
+	fmt.Fprintf(outputFile, `</body>
 </html>`)
+	return nil
+}
+
+func main() {
+	files, err := ioutil.ReadDir(inputDir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not read the input directory: %s", err.Error())
+		os.Exit(1)
+	}
+
+	for _, file := range files {
+		build(file.Name())
+	}
 }
