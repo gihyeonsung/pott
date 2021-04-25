ca471e3350765ec31457a5a3d727d1f59832cd60
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-04-25 13:29:59 +0900

    fix: change visibility of Render to public

--- internal/renderer/text_renderer.go
+++ internal/renderer/text_renderer.go
@@ -7,7 +7,7 @@ import (
 	"github.com/gihyeonsung/pott/internal/model"
 )
 
-func render(w io.Writer, doc *model.TextDocument) error {
+func Render(w io.Writer, doc *model.TextDocument) error {
 	w.Write([]byte("<pre>"))
 	if _, err := w.Write([]byte(doc.Content)); err != nil {
 		return errors.New("could not write the text document: " + err.Error())
