e6cac4e988b0279b22da8916c8d806d41908d7ce
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-04-25 11:34:18 +0900

    feat: create dummy text_parser.go

---
+++ internal/parser/text_parser.go
@@ -0,0 +1,12 @@
+package parser
+
+import (
+	"errors"
+	"io"
+
+	"github.com/gihyeonsung/pott/internal/model"
+)
+
+func parse(r io.Reader) (*model.TextDocument, error) {
+	return nil, errors.New("not implemented")
+}
