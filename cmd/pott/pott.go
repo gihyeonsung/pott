c9b73d5cd6ca4c87ac043b150775a371ae5c18f0
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-04-25 13:31:41 +0900

    feat: impl minimal program

--- cmd/pott/pott.go
+++ cmd/pott/pott.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/gihyeonsung/pott/internal/parser"
+	"github.com/gihyeonsung/pott/internal/renderer"
+)
 
 func main() {
-	fmt.Println("kabochan in pott")
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not read the document: %s", err.Error())
+		os.Exit(1)
+	}
+
+	doc, err := parser.Parse(file)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not parse the document: %s", err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Printf(`<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="index.css" type="text/css">
+</head>
+<body>`)
+	renderer.Render(os.Stdout, doc)
+	fmt.Printf(`</body>
+</html>`)
 }
