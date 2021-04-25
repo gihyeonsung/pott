42c34fcccb5cb556169746493c8a628b1f037757
Gihyeon Sung <gihyeon@gihyeon.xyz>
2021-04-25 11:50:20 +0900

    feat: impl text_parser.go

--- internal/model/types.go
+++ internal/model/types.go
@@ -3,3 +3,7 @@ package model
 type TextDocument struct {
 	content string
 }
+
+func NewTextDocument(content string) *TextDocument {
+	return &TextDocument{content: content}
+}
--- internal/parser/text_parser.go
+++ internal/parser/text_parser.go
@@ -3,10 +3,15 @@ package parser
 import (
 	"errors"
 	"io"
+	"io/ioutil"
 
 	"github.com/gihyeonsung/pott/internal/model"
 )
 
 func parse(r io.Reader) (*model.TextDocument, error) {
-	return nil, errors.New("not implemented")
+	content, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, errors.New("could not read the text document: " + err.Error())
+	}
+	return model.NewTextDocument(string(content)), nil
 }
