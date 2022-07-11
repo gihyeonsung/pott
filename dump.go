8a5c7299e498c6899603767e77180a1c317ef98c
Gihyeon Sung <gihyeon@gihyeon.xyz>
2022-07-11 19:56:13 +0900

    feat: css dump

--- dump.go
+++ dump.go
@@ -6,7 +6,21 @@ import (
 	"path/filepath"
 )
 
-func dump(c *category, out string) error {
+func dump(c *category, cssPath, out string) error {
+	css, err := os.ReadFile(cssPath)
+	if err != nil {
+		return err
+	}
+
+	cssPathOut := filepath.Join(out, filepath.Base(cssPath))
+	if err := os.WriteFile(cssPathOut, []byte(css), 0777); err != nil {
+		return err
+	}
+
+	return dumpCategory(c, out)
+}
+
+func dumpCategory(c *category, out string) error {
 	log.Printf("write: dumping c=%+v out=%+v", c, out)
 	dir := filepath.Join(out, c.name)
 	if err := os.MkdirAll(dir, 0777); err != nil {
@@ -21,7 +35,7 @@ func dump(c *category, out string) error {
 	}
 
 	for _, inner := range c.inners {
-		if err := dump(inner, dir); err != nil {
+		if err := dumpCategory(inner, dir); err != nil {
 			return err
 		}
 	}
