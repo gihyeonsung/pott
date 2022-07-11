cca6ef9b3364e9600bd87ead43f948a27b8f7ce5
Gihyeon Sung <gihyeon@gihyeon.xyz>
2022-07-11 15:44:39 +0900

    feat: flag

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
-const (
-	pathContent = "content"
-	pathCss     = "index.css"
-	pathBuild   = "build"
-	pathLayout  = "layout.tmpl"
+import (
+	"flag"
+	"log"
 )
 
 func main() {
+	pathContent := flag.String("content", "content", "contents location")
+	pathBuild := flag.String("output", "build", "output location")
+	pathLayout := flag.String("template", "layout.tmpl", "template location")
+	pathCss := flag.String("stylesheet", "index.css", "stylesheet location")
+	log.Printf("*pathContent=%+v", *pathContent)
+	log.Printf("*pathBuild=%+v", *pathBuild)
+	log.Printf("*pathLayout=%+v", *pathLayout)
+	log.Printf("*pathCss=%+v", *pathCss)
 }
