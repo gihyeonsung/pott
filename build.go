956d2a7c50062ea70ac3df4167f7b0780be87031
Gihyeon Sung <gihyeon@gihyeon.xyz>
2022-07-11 19:50:17 +0900

    feat: template

--- build.go
+++ build.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"html/template"
 	"log"
+	"os"
 	"strings"
 
 	"github.com/yuin/goldmark"
@@ -11,7 +13,37 @@ import (
 	"github.com/yuin/goldmark/text"
 )
 
-func buildDocument(d *document, layout, css string) error {
+func build(c *category, layoutPath, cssPath string) error {
+	layout, err := os.ReadFile(layoutPath)
+	if err != nil {
+		return err
+	}
+
+	tmpl, err := template.New("layout").Parse(string(layout))
+	if err != nil {
+		return err
+	}
+
+	return buildCategory(c, tmpl, cssPath)
+}
+
+func buildCategory(c *category, tmpl *template.Template, cssPath string) error {
+	for _, i := range c.inners {
+		if err := buildCategory(i, tmpl, cssPath); err != nil {
+			return err
+		}
+	}
+
+	for _, d := range c.docs {
+		if err := buildDocument(d, tmpl, cssPath); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func buildDocument(d *document, tmpl *template.Template, css string) error {
 	log.Printf("buildDocument: building d.name=%+v", d.name)
 
 	markdown := goldmark.New(goldmark.WithExtensions(meta.Meta))
@@ -26,11 +58,6 @@ func buildDocument(d *document, layout, css string) error {
 		}
 	}
 
-	var content strings.Builder
-	markdown.Renderer().Render(&content, d.raw, parsed)
-	d.content = content.String()
-
-	log.Printf("metamadata=%+v", metadata)
 	if created, ok := metadata["created"]; ok {
 		if created, ok := created.(string); ok {
 			d.created = created
@@ -43,21 +70,20 @@ func buildDocument(d *document, layout, css string) error {
 		}
 	}
 
-	return nil
-}
+	var body strings.Builder
+	markdown.Renderer().Render(&body, d.raw, parsed)
 
-func build(c *category, layout, css string) error {
-	for _, i := range c.inners {
-		if err := build(i, layout, css); err != nil {
-			return err
-		}
-	}
-
-	for _, d := range c.docs {
-		if err := buildDocument(d, layout, css); err != nil {
-			return err
-		}
-	}
+	var content strings.Builder
+	tmpl.Execute(&content, &struct {
+		Title   string
+		Created string
+		Body    template.HTML
+	}{
+		Title:   d.title,
+		Created: d.created,
+		Body:    template.HTML(body.String()),
+	})
+	d.content = content.String()
 
 	return nil
 }
