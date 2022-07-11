8a43aef50180e36079eb15954295af1022a4f397
Gihyeon Sung <gihyeon@gihyeon.xyz>
2022-07-11 19:27:35 +0900

    feat: title parsing

--- build.go
+++ build.go
@@ -6,20 +6,28 @@ import (
 
 	"github.com/yuin/goldmark"
 	"github.com/yuin/goldmark-meta"
+	"github.com/yuin/goldmark/ast"
 	"github.com/yuin/goldmark/parser"
 	"github.com/yuin/goldmark/text"
 )
 
 func buildDocument(d *document, layout, css string) error {
+	log.Printf("buildDocument: building d.name=%+v", d.name)
+
 	markdown := goldmark.New(goldmark.WithExtensions(meta.Meta))
 	ctx := parser.NewContext()
-	ast := markdown.Parser().Parse(text.NewReader(d.raw), parser.WithContext(ctx))
+	parsed := markdown.Parser().Parse(text.NewReader(d.raw), parser.WithContext(ctx))
 	metadata := meta.Get(ctx)
 
-	d.title = "no title"
+	if parsed.HasChildren() {
+		node := parsed.FirstChild()
+		if node.Kind() == ast.KindHeading {
+			d.title = string(node.Text(d.raw))
+		}
+	}
 
 	var content strings.Builder
-	markdown.Renderer().Render(&content, d.raw, ast)
+	markdown.Renderer().Render(&content, d.raw, parsed)
 	d.content = content.String()
 
 	log.Printf("metamadata=%+v", metadata)
